6fe4056cb674298ad0f9759b6d46f832831866db
gankris123 <ganiemodesto300@gmail.com>
2025-02-13 06:11:04 -0800

    java
    Added  Java Application

---
+++ Modesto_IT13A_Activities/src/modesto_it13a_activities/Modesto_IT13A_Activities.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modesto_it13a_activities;
+
+/**
+ *
+ * @author PC
+ */
+public class Modesto_IT13A_Activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
