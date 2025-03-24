edbd27fcddf7c69cb3c13c37963c0a00d696ad92
gankris123 <ganiemodesto300@gmail.com>
2025-03-25 07:44:11 +0800

    java application
    java file

---
+++ Modesto_IT13A_Activities/src/midterm/baldesco_modesto/Baldesco_Modesto.java
@@ -0,0 +1,12 @@
+package baldesco_modesto;
+
+    public class Baldesco_Modesto {
+
+    public static void main(String[] args) {
+        String[] array = {"A", "B", "C", "D", "E", "F", "G"};
+        for (String letter : array) {
+            System.out.print(letter + ",");
+        }
+    }
+}
+
